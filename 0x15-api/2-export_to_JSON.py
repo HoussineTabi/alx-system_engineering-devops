dd5437d299832e0363088cd923fe52f869e01ea2
HoussineTabi <0620036304ht@gmail.com>
2024-01-02 18:27:08 +0000

    task_2_correction

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""
-Data in The JSON format
-"""
-
+'''
+Data in the JSON format
+'''
 
 import json
 import requests
 from sys import argv
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     uid = argv[1]
-    url = "https://jsonplaceholder.typicode.com/{}".format(uid)
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
     user = requests.get(url, verify=False).json()
-    url = "https://jsonplaceholder.typicode.com/todos?usrId={}".format(uid)
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
     todo = requests.get(url, verify=False).json()
     name = user.get('username')
-    t = [{"taask": t.get("title"),
-        "username": name,
-        "completed": t.get("completed")} for t in todo]
-    bj = dict()
+    t = [{"task": t.get("title"),
+          "username": name,
+          "completed": t.get("completed")} for t in todo]
+    bj = {}
     bj[uid] = t
-    with open("{}.json".form(uid), 'w') as filejs:
+    with open("{}.json".format(uid), 'w') as filejs:
         json.dump(bj, filejs)
