847866e13271e2ee12e913f303959459f940bdee
HoussineTabi <0620036304ht@gmail.com>
2024-01-16 20:06:37 +0000

    tasks_0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -11,10 +11,6 @@ def number_of_subscribers(subreddit):
         return 0 if invalid subreddit given
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    # get user agent
-    # https://stackoverflow.com/questions/10606133/ -->
-    # sending-user-agent-using-requests-library-in-python
     headers = requests.utils.default_headers()
     headers.update({'User-Agent': 'My User Agent 1.0'})
 
