d59aee61f4046b015210ebc78d8259a0aa946cab
HoussineTabi <0620036304ht@gmail.com>
2024-01-16 18:51:06 +0000

    Correction_task_0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,29 +1,14 @@
-#!/usr/ben/python3
-"""
-Task number 0
-"""
-
+#!/usr/bin/python3
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Returns the number of subscribers
-    """
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    headers = {'User-Agent': 'MyAPIUserAgent'}
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;\
+                         rv:68.0) Gecko/20100101 FirefoxFirefox/68.0'}
+    with requests.session() as client:
+        info = client.get(url, headers=headers, allow_redirects=False).json()
+        try:
+            return info.get('data', {}).get('subscribers', 0)
+        except Exception:
             return 0
-    except:
-        return 0
-
-
-if __name__ == "__main__":
-    print()
